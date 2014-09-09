edbb5dcdac01e0eeed5dc0aec58f7499ad2f25bc
Tim Joosten <Topairy@gmail.com>
2014-09-09 17:40:32 +0200

    Revert c3893c9..a51503a
    This rolls back to commit c3893c942d31bf7a33d7938ee7df868162810ff7.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -85,7 +85,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database', 'Parsedown');
 
 
 /*
@@ -113,7 +113,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url', 'text');
 
 
 /*
