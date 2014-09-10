2dec01bb962bfac3e35b97592b4501de5b60312b
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-09-10 20:54:10 +0200

    Create Model_verhuur.php

---
+++ application/models/Model_verhuur.php
@@ -0,0 +1,11 @@
+<?php 
+  class Model_verhuur extends CI_model {
+    function Insert_verhuur() {
+    }
+    
+    function Verhuur_Bevestigd() {
+    }
+    
+    function Verhuur_Optie() {
+    }
+  }
