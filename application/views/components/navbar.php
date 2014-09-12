b4e15d0ecd752e9e8467d05fab93b4a7f2e6f567
Tim Joosten <Topairy@gmail.com>
2014-09-12 23:08:28 +0200

    update

---
+++ application/views/components/navbar.php
@@ -0,0 +1,67 @@
+      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Sint-Joris</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="<?php echo base_url(); ?>Takken">
+                  <span class="fa fa-leaf Icon-color"></span> Takken
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="<?php echo base_url(); ?>Takken/Kapoenen"><span class="fa fa-chevron-right"></span> De Kapoenen</a></li>
+                  <li><a href="<?php echo base_url(); ?>Takken/Welpen"><span class="fa fa-chevron-right"></span> De Welpen</a></li>
+                  <li><a href="<?php echo base_url(); ?>Takken/JongGivers"><span class="fa fa-chevron-right"></span> De Jong-givers</a></li>
+                  <li><a href="<?php echo base_url(); ?>Takken/Givers"><span class="fa fa-chevron-right"></span> De Givers</a></li>
+                  <li><a href="<?php echo base_url(); ?>Takken/Jins"><span class="fa fa-chevron-right"></span> De Jins</a></li>
+                  <li><a href="<?php echo base_url(); ?>Takken/Leiding"><span class="fa fa-chevron-right"></span> De Leiding</a></li>
+                </ul>
+              </li>
+
+              <li>
+                <a href="<?php echo base_url(); ?>Verhuur">
+                  <span class="fa fa-home Icon-color"></span> Verhuur
+                </a>
+              </li>
+
+              <li>
+                <a href="<?php echo base_url(); ?>Fotos">
+                  <span class="fa fa-camera-retro Icon-color"></span> Foto's
+                </a>
+              </li>
+
+              <li>
+                <a href="<?php echo base_url(); ?>">
+                  <span class="fa fa-file-text-o Icon-color"></span> Planning
+                </a>
+              </li>
+
+              <li>
+                <a href="<?php echo base_url(); ?>Info">
+                  <span class="fa fa-info-circle Icon-color"></span> Info
+                </a>
+              </li>
+
+              <li>
+                <a href="<?php echo base_url();?>">
+                  <span class="fa fa-calendar Icon-color"></span> Activiteiten
+                </a>
+              </li>
+
+              <li>
+                <a href="<?php echo base_url(); ?>">
+                  <span class="fa fa-envelope Icon-color"></span> Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
