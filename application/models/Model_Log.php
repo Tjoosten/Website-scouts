6083860deb7141abb9dc140fdcb3df97cf932bf1
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-09-24 19:15:55 +0200

    Update Model_Log.php

--- application/models/Model_Log.php
+++ application/models/Model_Log.php
@@ -1,37 +1,55 @@
 <?php
 
-	/*
-	 * Developer: Tim Joosten
-	 * License: 4GPL
-	 * Copyright: Sint-Joris Turnhout, Tim Joosten
-	 */
+  /*
+   * Developer: Tim Joosten
+   * License: 4GPL
+   * Copyright: Sint-Joris Turnhout, Tim Joosten
+   */
 
-	Class Model_Log extends CI_Model {
-		function Logged_in() {
+  Class Model_Log extends CI_Model {
+    function Logged_in() {
+      $Values = array(
+			  "Gebruiker" => $Session['username'], 
+			  "Bericht"   => "Heeft zich ingelogd", 
+			  "Datum"     => date("F j, Y, g:i a"),
+			 );
 			
-		}
+			$this->db->insert('Activity_log', $Values);
+    }
 		
-		function Logged_out() {
+    function Logged_out() {
+      $Values = array(
+			  "Gebruiker" => $Session['username'],
+			  "Bericht"   => "Heeft zich uitgelogd",
+			  "Datum"     => date("F j, Y, g:i a"),
+			);
 			
+			$this->db->insert('Activity_log', $Values);
 		}
 		
 		function Created_login() {
-		
+		  $Values = array(
+		    "Gebruiker" => $Session['username'],
+		    "Bericht"   => "Heeft een login toegegevoegd",
+		    "Datum"     => date("F j, Y, g:i a"),
+		  );
+		  
+		  $this->db->insert('Activity_log', $Values);
 		}
 		
 		function Deleted_login()  {
 			$Values = array(
-				"Gebruiker" =>
-				"Bericht"   =>
-				"Datum"     =>
+				"Gebruiker" => $Session['username'],
+				"Bericht"   => "Heeft een login verwijderd",
+				"Datum"     => date("F j, Y, g:i a"),
  			);
 		}
 		
 		function Add_admin() {
 			$Values = array(
-				"Gebruiker" =>
-				"Bericht"   =>
-				"Datum"     =>
+				"Gebruiker" => $Session['username'],
+				"Bericht"   => "Heeft iemand administrator gemaakt",
+				"Datum"     => date("F j, Y, g:i a"),
 				);
 				
 			$this->db->insert('Activity_log', $Values);
@@ -39,9 +57,9 @@
 		
 		function Delete_admin() {
 			$Values = array(
-				"Gebruiker" =>
-				"Bericht"   =>
-				"Datum"     =>
+				"Gebruiker" => $Session['username'],
+				"Bericht"   => "Heeft iemand verwijderd als administrator",
+				"Datum"     => date("F j, Y, g:i a"),
 				);
 				
 			$this->db->insert('Activity_log', $Values);
@@ -49,9 +67,9 @@
 		
 		function Make_user() {
 			$Values = array(
-				"Gebruiker" =>
-				"Bericht"   =>
-				"Datum"     =>
+				"Gebruiker" => $Session['username'],
+				"Bericht"   => "Heeft een login aangemaakt",
+				"Datum"     => date("F j, Y, g:i a"),
 				);
 				
 			$this->db->insert('Activity_log', $Values);
