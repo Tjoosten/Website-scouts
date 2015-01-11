45ff6ea1d5ab27866745644a8703860cfdf989f4
Tim Joosten <Topairy@gmail.com>
2015-01-11 15:42:53 +0100

    inject logged_in session to front page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 	class Welcome extends CI_Controller {
+		public $Session = array();
+
 		function __construct() {
 			parent::__construct();
+			$this->Session = $this->session->userdata('logged_in');
 		}
 
 		public function index() {
@@ -9,7 +12,9 @@
 			$this->output->cache(5);
 			// END
 
+			// Not in array, because it's one variable.
 			$Data['Title'] = "Index";
+
 			$this->load->view('client/index', $Data);
 		}
 	}
