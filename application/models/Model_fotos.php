a649bbd40276e89b300ca4a8feda2af09886c523
Tjoosten <Tjoosten3@gmail.com>
2015-01-13 14:15:53 +0100

    add docblock

--- application/models/Model_fotos.php
+++ application/models/Model_fotos.php
@@ -3,11 +3,11 @@
 		function Select() {
 			$this->db->select()
 					     ->limit(9);
-			
+
 			$Query = $this->db->get('Photo_Gallery');
 			return $Query->result();
 		}
-		
+
 		function Select_tak() {
 			$this->db->select()
 					  ->where('Tak', $this->uri->segment(3));
@@ -19,26 +19,30 @@
 		function Insert($image_data = array()) {
 			$Values = array(
 				"Naam"      => $this->input->post('Naam'),
-		    "Tak"       => $this->input->post('Tak'),			
+		    "Tak"       => $this->input->post('Tak'),
 				"Web_url"   => $this->input->post('URL'),
 				"File_path" => "/assets/fotos/".$image_data['file_name'],
 				"File_name" => $image_data['file_name'],
 			);
-			
+
 			$this->db->insert('Photo_Gallery', $Values);
-			return $this->db->affected_rows(); 
+			return $this->db->affected_rows();
 		}
-		
+
 		function Backend_select() {
 			$this->db->select()
 					     ->limit(25);
-			
+
 			$Query = $this->db->get('Photo_Gallery');
 			return $Query->result();
 		}
-		
+
+		/**
+		 * Deletes a photo in the database
+		 */
+
 		function Delete() {
       $this->db->where('File_name', $this->uri->segment(3))
          	     ->delete('Photo_Gallery');
 		}
-	} 
+	}
