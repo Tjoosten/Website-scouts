c0f9fcda74464fb652626fe07be20d8dbf1c1911
Tjoosten <Tjoosten3@gmail.com>
2015-07-31 15:38:11 +0200

    Implement permissions session to the mailinglist controller.
    Modified: application/controllers/Mailing.php

--- application/controllers/Mailing.php
+++ application/controllers/Mailing.php
@@ -1,149 +1,168 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-	/**
-	 *
-	 */
+/**
+ *
+ */
+class Mailing extends CI_Controller
+{
+    public $Permissions   = array();
+    public $Session       = array();
+    public $Error_heading = array();
+    public $Error_message = array();
 
-	class Mailing extends CI_Controller {
-		public $Permissions   = array();
-		public $Session       = array();
-		public $Error_heading = array();
-		public $Error_message = array();
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Model_mailing', 'Mailing');
+        $this->load->helper(array('Email', 'logger'));
+        $this->load->library(array('Email'));
 
-  	function __construct() {
-      parent::__construct();
-      $this->load->model('Model_mailing','Mailing');
-      $this->load->helper(array('Email', 'logger'));
-      $this->load->library(array('Email'));
-
-			$this->Session = $this->session->userdata('logged_in');
-		$this->Permissions = $this->session->userdata('Permissions');
+        $this->Session     = $this->session->userdata('logged_in');
+        $this->Permissions = $this->session->userdata('Permissions');
     }
-		// END constructor
+    // END constructor
 
-		/**
-		 * Index for mailing backend
-		 */
-		public function index() {
-			if($this->Session && $this->Permissions) {
-				if($this->Session['Admin'] == 1) {
-					$Data = array(
-						'Title'   => 'Mailing',
-						'Active'  => '6',
-						'Mailing' => $this->Mailing->Mailing(),
-					);
+    /**
+     * Index for mailing backend
+     */
+    public function index()
+    {
+        if ($this->Session && $this->Permissions) {
+            if ($this->Session['Admin'] == 1 || $this->Permissions['mailinglist'] == 'Y') {
+                $Data = array(
+                    'Title' => 'Mailing',
+                    'Active' => '6',
+                    'Mailing' => $this->Mailing->Mailing(),
+                );
 
-					$this->load->view('components/admin_header', $Data);
-					$this->load->view('components/navbar_admin', $Data);
-					$this->load->view('admin/Mailing', $Data);
-					$this->load->view('components/footer');
-				} else {
-					$Data['Heading'] = $this->Error_heading;
-					$Data['Message'] = $this->Error_message;
+                $this->load->view('components/admin_header', $Data);
+                $this->load->view('components/navbar_admin', $Data);
+                $this->load->view('admin/Mailing', $Data);
+                $this->load->view('components/footer');
+            } else {
+                $Data['Heading'] = $this->Error_heading;
+                $Data['Message'] = $this->Error_message;
 
-					$this->load->view('errors/html/alert', $Data);
-				}
-			} else {
-				redirect('Admin','Refresh');
-			}
-		}
+                $this->load->view('errors/html/alert', $Data);
+            }
+        } else {
+            redirect('Admin', 'Refresh');
+        }
+    }
 
-		/**
-		 *
-		 */
-		public function Mail() {
-			$List = $this->input->post('List');
+    /**
+     *
+     */
+    public function Mail()
+    {
+        if ($this->Session && $this->Permissions) {
+            if ($this->Session['admin'] == 1 || $this->Permissions['mailinglist'] == 'Y') {
+                $List = $this->input->post('List');
 
-			// Switch statement
-			switch ($List) {
-   			case "Iedereen":
-     			$Query = $this->Mailing->Mailing_Iedereen();
-     				break;
+                // Switch statement
+                switch ($List) {
+                    case "Iedereen":
+                        $Query = $this->Mailing->Mailing_Iedereen();
+                        break;
 
-   			case "VZW":
-     			$Query = $this->Mailing->Mailing_VZW();
-     				break;
+                    case "VZW":
+                        $Query = $this->Mailing->Mailing_VZW();
+                        break;
 
-     		case "Ouders":
-     			$Query = $this->Mailing->Mailing_Ouders();
-     				break;
+                    case "Ouders":
+                        $Query = $this->Mailing->Mailing_Ouders();
+                        break;
 
-     		case "Leiding":
-     			$Query = $this->Mailing->Mailing_Leiding();
-     				break;
+                    case "Leiding":
+                        $Query = $this->Mailing->Mailing_Leiding();
+                        break;
 
-     		case "Oudervergadering":
-     			$Query = $this->Mailing->Mailing_Oudervergadering();
-     				break;
+                    case "Oudervergadering":
+                        $Query = $this->Mailing->Mailing_Oudervergadering();
+                        break;
 
-   			default:
-     			echo "Kan niet bepalen naar welke lijst hij de mails moet sturen ;-(";
-					die();
-				}
+                    default:
+                        echo "Kan niet bepalen naar welke lijst hij de mails moet sturen ;-(";
+                        die();
+                }
 
-			$text = $this->input->post('Message');
+                $text = $this->input->post('Message');
 
-			// Mail message
-      foreach($Query as $Output) {
-      	$this->email->set_mailtype("html");
-        $this->email->from('Mailing@st-joris-turnhout.be', 'Mailing st-joris turnhout');
-        $this->email->to($Output->Email);
-        $this->email->subject($this->input->post('subject'));
-        $this->email->message(Parsedown::instance()->parse($text)); // Pasedown???? Bug possible!
-        $this->email->send();
-        $this->email->clear();
-      }
+                // Mail message
+                foreach ($Query as $Output) {
+                    $this->email->set_mailtype("html");
+                    $this->email->from('Mailing@st-joris-turnhout.be', 'Mailing st-joris turnhout');
+                    $this->email->to($Output->Email);
+                    $this->email->subject($this->input->post('subject'));
+                    $this->email->message(Parsedown::instance()->parse($text)); // Pasedown???? Bug possible!
+                    $this->email->send();
+                    $this->email->clear();
+                }
 
-      redirect('Mailing');
-		}
+                redirect('Mailing');
+            }
+        } else {
+            redirect('Admin', 'refresh')
+        }
+    }
 
-		/**
-		 * Voeg een email adress toe
-		 */
-		public function Add_address() {
-			if($this->Session) {
-				$this->Mailing->Insert_address();
-				user_log($this->Session['username'], 'Heeft een adres toegevoegd aan de mailinglist.');
-				redirect('Mailing');
-			} else {
-				redirect('Admin', 'refresh');
-			}
-		}
+    /**
+     * Voeg een email adress toe
+     */
+    public function Add_address()
+    {
+        if ($this->Session && $this->Permissions) {
+            if ($this->Session['admin'] == 1 || $this->Permissions['mailinglist'] == 'Y') {
+                $this->Mailing->Insert_address();
+                redirect('Mailing');
+            }
+        } else {
+            redirect('Admin', 'refresh');
+        }
+    }
 
-		/**
-		 * Delete een email adres.
-		 */
-		public function Delete_address() {
-			if($this->Session) {
-				$this->Mailing->Delete_address();
-				user_log($this->Session['username'], 'Heeft een adres verwijderd uit de mailinglists.');
-				redirect('Mailing');
-			} else {
-				redirect('Admin', 'refresh');
-			}
-		}
+    /**
+     * Delete een email adres.
+     */
+    public function Delete_address()
+    {
+        if ($this->Session) {
+            if ($this->Session['admin'] == 1 || $this->Permissions['mailinglist'] == 'Y') {
+                $this->Mailing->Delete_address();
+                redirect('Mailing');
+            }
+        } else {
+            redirect('Admin', 'refresh');
+        }
+    }
 
-		/**
-		 *
-		 */
-		public function Inactief() {
-			if($this->Session) {
-				$this->Mailing->Inactief();
-				redirect('Mailing');
-			} else {
-				redirect('Admin','refresh');
-			}
-		}
+    /**
+     *
+     */
+    public function Inactief()
+    {
+        if ($this->Session && $this->Permissions) {
+            if ($this->Session['admin'] == 1 || $this->Permissions['mailinglist'] == 'Y') {
+                $this->Mailing->Inactief();
+                redirect('Mailing');
+            }
+        } else {
+            redirect('Admin', 'refresh');
+        }
+    }
 
-		/**
-		 *
-		 */
-		public function Actief() {
-			if($this->Session) {
-				$this->Mailing->Actief();
-				redirect('Mailing');
-			} else {
-				redirect('Admin', 'refresh');
-			}
-		}
-	}
+    /**
+     *
+     */
+    public function Actief()
+    {
+        if ($this->Session && $this->Permissions) {
+            if ($this->Session['admin'] == 1 || $this->Permissions['mailinglist'] == 'Y') {
+                $this->Mailing->Actief();
+                redirect('Mailing');
+            }
+        } else {
+            redirect('Admin', 'refresh');
+        }
+    }
+}
